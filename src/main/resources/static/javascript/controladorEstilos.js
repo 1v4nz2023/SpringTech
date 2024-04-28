b9bcabffdfb70d6f98807c35051bf26f04181bb1
1v4nz2023 <ivanmera0102102124@gmail.com>
2024-04-27 20:30:21 -0500

    20:30

--- src/main/resources/static/javascript/controladorEstilos.js
+++ src/main/resources/static/javascript/controladorEstilos.js
@@ -1,18 +1,15 @@
 
-
-// CONTROL DE SOMBRAS Y OPACIDAD DE LAS IMAGENES
-$(document).ready(function(){
-    // Al pasar el ratón sobre la imagen, agrega sombreado
-    $('.card-img-top').mouseenter(function(){
+$(document).ready(function() {
+    // Agregar efecto de sombreado a las imágenes al pasar el ratón
+    $(document).on('mouseenter', '.card-img-top', function() {
         $(this).addClass('image-shadow'); // Agregar clase para sombreado
         $(this).addClass('image-opaque'); // Agregar clase para opacidad
-
     });
 
-    // Al quitar el ratón de la imagen, quita el sombreado
-    $('.card-img-top').mouseleave(function(){
+    // Quitar efecto de sombreado a las imágenes al quitar el ratón
+    $(document).on('mouseleave', '.card-img-top', function() {
         $(this).removeClass('image-shadow'); // Quitar clase para sombreado
         $(this).removeClass('image-opaque'); // Quitar clase para opacidad
-
     });
+
 });
---
+++ src/main/resources/static/javascript/domnewproducts.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function() {
+
+    // Obtener el contenedor donde se agregarán las tarjetas
+    var newproductContainer = $('#newproduct-container');
+    var newproductContainer2 = $('#newproduct-container2');
+
+    // Crear 5 tarjetas con ids distintos
+    for (var i = 1; i <= 5; i++) {
+        // Plantilla de cadena con el HTML de la tarjeta
+        var cardHtml = `
+            <div class="card col-md-2 col-4 mx-1 mt-2" style="width: 15rem" id="card${i}">
+                <a href=""><img src="../static/images/producto1.jpg" class="d-block w-100 card-img-top" alt="Producto ${i}"></a>
+                <div class="card-body">
+                    <h6 class="card-title">Product ${i}</h6>
+                    <div class="d-flex justify-content-between">
+                        <div class="d-flex flex-column product-text">
+                            <span >Precio</span>
+                            <span>Stock</span>
+                            <span>Referencia</span>
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <a href="#" class="btn btn-success product-text">Comprar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        var card2Html = `
+        <div class="card col-md-2 col-4 mx-1 mt-2" style="width: 15rem" id="card${i+5}">
+            <a href=""><img src="../static/images/producto1.jpg" class="d-block w-100 card-img-top" alt="Producto ${i+5}"></a>
+            <div class="card-body">
+                <h6 class="card-title">Product ${i+5}</h6>
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-column product-text">
+                        <span >Precio</span>
+                        <span>Stock</span>
+                        <span>Referencia</span>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <a href="#" class="btn btn-success product-text">Comprar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+        // Convertir la cadena HTML en un objeto jQuery
+        var $card = $(cardHtml);
+        var $card2 = $(card2Html);
+
+        // Agregar la tarjeta al contenedor
+        newproductContainer.append($card);
+        newproductContainer2.append($card2);
+
+    }
+});
